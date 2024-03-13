4b164d3521b4697c299278c9d5ad9a96bd685ae0
Yuri <+>
2024-03-12 23:47:24 -0400

    Completed Python Functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print('Hello, programmer!')
     pass
 
 def greet(name):
+    print(f'Hello, {name}!')
     pass
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}!')
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number/2
